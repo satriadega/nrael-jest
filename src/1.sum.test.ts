dc0888180c6b8393f7647718e0c0817fbb21c8ec
Egi <satria.pandega@gmail.com>
2025-05-18 09:59:17 +0700

    testing fail

--- src/1.sum.test.ts
+++ src/1.sum.test.ts
@@ -4,4 +4,9 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+// this will fail
+test("SHOULD BE FAILED, adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(5);
+});
+
 export {};
